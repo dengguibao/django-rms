0efe08f30281b8523ae321529794a2db4b386d21
dengguibao <tydgb@outlook.com>
2020-03-16 15:44:12 +0800

    update file upload function

--- admin/file_view.py
+++ admin/file_view.py
@@ -16,7 +16,6 @@ upload_path = 'uploads'
 def upload_file(request):
     if request.method == 'POST':
         local_path = os.path.join(settings.BASE_DIR, upload_path, today)
-        print(os.path.exists(local_path))
         if os.path.exists(local_path) is False:
             # os.chdir(upload_path)
             os.makedirs(local_path)
@@ -27,7 +26,7 @@ def upload_file(request):
         file_type = 1
         real_path = os.path.join(upload_path, today)
         name = origin_file_obj.name
-
+        file_size = origin_file_obj.size
         file_ext = name.split('.')[-1]
         # new file name
         real_name = time.strftime(
@@ -45,6 +44,7 @@ def upload_file(request):
             'path': path,
             'owner': user_obj,
             'type': file_type,
+            'file_size': format_file_size(file_size),
             'file_type': file_ext,
             'real_name': real_name,
             'real_path': real_path
@@ -56,6 +56,7 @@ def upload_file(request):
                 'data': {
                     'id': res.id,
                     'name': name,
+                    'file_size': res.file_size,
                     'real_name': real_name,
                     'real_path': real_path,
                     'pub_date': res.pub_date
@@ -104,7 +105,6 @@ def create_folder(request):
     p_path = request.POST.get('path','/')
 
     folder = FileInfo.objects.filter(name=folder_name, path=p_path)
-    print(folder)
     if folder:
         return JsonResponse({
             'code':1,
@@ -168,15 +168,39 @@ def file_delete(request, i):
 
 @login_required
 def file_download(request, id):
-    res=FileInfo.objects.get(id=id)
+    res = FileInfo.objects.get(id=id)
+    down = request.GET.get('d', 0)
     if res and res.owner != request.user:
         return render(request, 'admin/error.html')
+
+    if res.file_type == 'md':
+        temp_name = 'file_view_md.html'
     else:
-        try:
-            file = open('/'.join([res.real_path, res.real_name]), 'rb')
-            response = FileResponse(file)
-            response['Content-Type'] = 'application/octet-stream'
-            response['Content-Disposition'] = 'attachment;filename="{}"'.format(res.name.encode('utf-8').decode('ISO-8859-1'))
-            return response
-        except Exception:
-            raise Http404
+        temp_name = 'file_view_text.html'
+
+    if res.file_type in ['md', 'conf', 'log', 'txt', 'desktop', 'sh', 'py', 'php', 'js', 'css'] and down == 0:
+        with open('/'.join([res.real_path, res.real_name]), 'r', encoding='utf-8', errors="ignore") as f:
+            content = f.read()
+        return render(request, 'admin/%s' % (temp_name), {
+            'filename':res.name,
+            'content':content
+        })
+    try:
+        file = open('/'.join([res.real_path, res.real_name]), 'rb')
+        response = FileResponse(file)
+        response['Content-Type'] = 'application/octet-stream'
+        response['Content-Disposition'] = 'attachment;filename="{}"'.format(res.name.encode('utf-8').decode('ISO-8859-1'))
+        return response
+    except Exception:
+        raise Http404
+
+
+def format_file_size(size):
+    if size < 1024:
+        return '%i' % size + 'B'
+    elif 1024 <= size < 1024**2:
+        return '%.1f' % float(size/1024) + 'K'
+    elif 1024**2 <= size < 1024**3:
+        return '%.1f' % float(size/1024**2) + 'M'
+    elif 1024**3 <= size < 1024**4:
+        return '%.1f' % float(size/1024**3) + 'G'
