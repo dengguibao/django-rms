c1c340b77bc0f571bc43b4b16cefb76ce7f9dd21
dengguiao <tydgb@outlook.com>
2020-06-02 20:01:14 +0800

    10086 poll

--- app/scripts/10086_eva.py
+++ app/scripts/10086_eva.py
@@ -1,13 +1,15 @@
+#! coding=utf-8
 import requests
 import re
 import time
+import json
 import urllib3
 
 urllib3.disable_warnings()
 
 proxies = {
-    'http': 'http://172.31.10.84:2345',
-    'https': 'http://172.31.10.84:2345'
+#    'http': 'http://172.31.10.84:2345',
+#    'https': 'http://172.31.10.84:2345'
 }
 
 req_headers = {
@@ -30,89 +32,91 @@ if len(proxies):
 
 def submit_poll(url):
     r = s.get(url, verify=False)
+    if '重复提交' in r.text:
+        return '%s 链接已提交' % url
     if '请求超时' in r.text:
         return '%s 链接已超时' % url
 
     jump302_url = r.url
     args = get_url_args(jump302_url)
 
+    score_data = {
+        "qnrSuggestion": "",
+        "questionList": [
+            {
+                "score": 10,
+                "questionId": "2019112714221718701051001",
+                "questionTitle": u"故障维修整体满意度",
+                "answerOrder": 1,
+                "surveyObjType": "0",
+                "suggestion": "",
+                "surveyLables": ""
+            },
+            {
+                "score": 10,
+                "questionId": "2019112714472810201052001",
+                "questionTitle": u"上门及时性",
+                "answerOrder": 2,
+                "surveyObjType": "0",
+                "suggestion": "",
+                "surveyLables": u"上门快,上门准时,有提前预约时间"
+            },
+            {
+                "score": 10,
+                "questionId": "2019112714481805801051001",
+                "questionTitle": u"故障解决专业性",
+                "answerOrder": 3,
+                "surveyObjType": "0",
+                "suggestion": "",
+                "surveyLables": u"故障解决快,现场干净整洁"
+            },
+            {
+                "score": 10,
+                "questionId": "2019112714504311401052001",
+                "questionTitle": u"维修人员服务",
+                "answerOrder": 4,
+                "surveyObjType": "0",
+                "suggestion": "",
+                "surveyLables": u"服务态度好,答疑指导耐心,工装整洁大方"
+            },
+            {
+                "score": "",
+                "questionId": "2019112714514659501053001",
+                "questionTitle": u"维修完成后是否测速",
+                "suggestion": "",
+                "surveyLables": u"是",
+                "surveyObjType": "0",
+                "answerOrder": 5
+            }
+        ]
+    }
     post_data = {
-        "questionList": {
-            "qnrSuggestion": "",
-            "questionList": [
-                {
-                    "score": 10,
-                    "questionId": "2019112714221718701051001",
-                    "questionTitle": "故障维修整体满意度",
-                    "answerOrder": 1,
-                    "surveyObjType": "0",
-                    "suggestion": "",
-                    "surveyLables": ""
-                },
-                {
-                    "score": 10,
-                    "questionId": "2019112714472810201052001",
-                    "questionTitle": "上门及时性",
-                    "answerOrder": 2,
-                    "surveyObjType": "0",
-                    "suggestion": "",
-                    "surveyLables": "上门快,上门准时,有提前预约时间"
-                },
-                {
-                    "score": 10,
-                    "questionId": "2019112714481805801051001",
-                    "questionTitle": "故障解决专业性",
-                    "answerOrder": 3,
-                    "surveyObjType": "0",
-                    "suggestion": "",
-                    "surveyLables": "故障解决快,现场干净整洁"
-                },
-                {
-                    "score": 10,
-                    "questionId": "2019112714504311401052001",
-                    "questionTitle": "维修人员服务",
-                    "answerOrder": 4,
-                    "surveyObjType": "0",
-                    "suggestion": "",
-                    "surveyLables": "服务态度好,答疑指导耐心,工装整洁大方"
-                },
-                {
-                    "score": "",
-                    "questionId": "2019112714514659501053001",
-                    "questionTitle": "维修完成后是否测速",
-                    "suggestion": "",
-                    "surveyLables": "是",
-                    "surveyObjType": "0",
-                    "answerOrder": 5
-                }
-            ]
-        },
+        "questionList": json.dumps(score_data, ensure_ascii=False),
         "serNum": '',
         "channelId": '',
         "serType": '',
         "prvo": '',
         "servalue": '',
-        "detailsUrl": '/survey/detailsofTomatoOrange.html?taskSheetId='
+        "detailsUrl": '/survey/detailsofTomatoOrange.html?taskSheetId=',
+        'qnrId': args['questionnaireId'],
+        'h5PageUrlWeb': 'https://eva.customer.10086.cn/surv/QnrTomatoOrange.html?questionnaireId=%s' % args['questionnaireId'],
+        'token': args['token']
     }
 
-    post_data['qnrId'] = args['questionnaireId']
-    post_data['h5PageUrlWeb'] = 'https://eva.customer.10086.cn/surv/QnrTomatoOrange.html?questionnaireId=%s' % args['questionnaireId']
-    post_data['token'] = args['token']
-
-    print(post_data)
     req_headers['Referer'] = jump302_url
     s.headers.update(req_headers)
+
     r = s.post(
         'https://eva.customer.10086.cn/surveyH5Response/dynamicQuestion',
-        json=post_data,
+        data=post_data,
         verify=False
     )
     if r.status_code == 200:
         del req_headers['Referer']
-        return 'msg:success, status_code:%s, url:%s' % (r.status_code, url)
+        return 'msg:success, status_code:%s, url:%s, return_text:%s' % (r.status_code, url, r.text)
         #return r.content
     else:
-        return 'msg:failed, status_code:%s, url:%s' % (r.status_code, url)
+        return 'msg:failed, status_code:%s, url:%s, return_text:%s' % (r.status_code, url, r.text)
 
 
 def get_url_args(url):
@@ -133,26 +137,26 @@ if __name__ == "__main__":
     fmt = '%Y/%m/%d %H:%M'
 
     with open(file_name, 'r', encoding='utf-8') as f:
-        lines = f.read().replace('	', '|')
+        text_data = f.read().replace('	', '|')
 
-    lines = re.sub(' +', ' ', lines)
-    if not len(lines):
+    text_data = re.sub(' +', ' ', text_data)
+    if not len(text_data):
         exit('file is empty')
 
-    lines = lines.split('\n')
+    lines = text_data.split('\n')
     while True:
         if not len(lines):
             exit('done')
-
         for line in lines:
-            if line.strip() == ' ':
+            if line.strip() == '':
+                lines.pop(0)
                 continue
+
             x = line.split('|')
             now = int(time.time())
             end_date = time.mktime(time.strptime(x[0], fmt))
             #print (end_date-now)
             if end_date-now <= pre_time*60:
-                r = submit_poll(x[1])
-                print(r)
-                # print(line)
+                resp = submit_poll(x[1])
+                print(resp)
                 lines.pop(0)
