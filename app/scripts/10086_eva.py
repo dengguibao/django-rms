5b7e89f62e289d1a492234e1a1b4c0c98528f376
dengguibao <tydgb@outlook.com>
2020-06-04 17:40:46 +0800

    update 10086 pool to pyqt5 programe

--- app/scripts/10086_eva.py
+++ app/scripts/10086_eva.py
@@ -1,162 +1,292 @@
-#! coding=utf-8
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QMessageBox
+import sys
+
 import requests
 import re
 import time
 import json
 import urllib3
-
+import _thread
 urllib3.disable_warnings()
 
-proxies = {
-#    'http': 'http://172.31.10.84:2345',
-#    'https': 'http://172.31.10.84:2345'
-}
-
-req_headers = {
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept': 'application/json, text/javascript, */*; q=0.01,',
-    'Connection': 'Keep-Alive',
-    'Host': 'eva.customer.10086.cn',
-    'Origin': 'https://eva.customer.10086.cn',
-    #'Referer': 'https://eva.customer.10086.cn/survey/QnrTomatoOrange.html?token=covfnGb69714&&questionnaireId=2019112714550104601052001&&tokenflag=3f43a658',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest'
-}
-s = requests.session()
-s.headers.update(req_headers)
-if len(proxies):
-    s.proxies = proxies
-
-
-def submit_poll(url):
-    r = s.get(url, verify=False)
-    if '重复提交' in r.text:
-        return '%s 链接已提交' % url
-    if '请求超时' in r.text:
-        return '%s 链接已超时' % url
-
-    jump302_url = r.url
-    args = get_url_args(jump302_url)
-
-    score_data = {
-        "qnrSuggestion": "",
-        "questionList": [
-            {
-                "score": 10,
-                "questionId": "2019112714221718701051001",
-                "questionTitle": u"故障维修整体满意度",
-                "answerOrder": 1,
-                "surveyObjType": "0",
-                "suggestion": "",
-                "surveyLables": ""
-            },
-            {
-                "score": 10,
-                "questionId": "2019112714472810201052001",
-                "questionTitle": u"上门及时性",
-                "answerOrder": 2,
-                "surveyObjType": "0",
-                "suggestion": "",
-                "surveyLables": u"上门快,上门准时,有提前预约时间"
-            },
-            {
-                "score": 10,
-                "questionId": "2019112714481805801051001",
-                "questionTitle": u"故障解决专业性",
-                "answerOrder": 3,
-                "surveyObjType": "0",
-                "suggestion": "",
-                "surveyLables": u"故障解决快,现场干净整洁"
-            },
-            {
-                "score": 10,
-                "questionId": "2019112714504311401052001",
-                "questionTitle": u"维修人员服务",
-                "answerOrder": 4,
-                "surveyObjType": "0",
-                "suggestion": "",
-                "surveyLables": u"服务态度好,答疑指导耐心,工装整洁大方"
-            },
-            {
-                "score": "",
-                "questionId": "2019112714514659501053001",
-                "questionTitle": u"维修完成后是否测速",
-                "suggestion": "",
-                "surveyLables": u"是",
-                "surveyObjType": "0",
-                "answerOrder": 5
-            }
-        ]
-    }
-    post_data = {
-        "questionList": json.dumps(score_data, ensure_ascii=False),
-        "serNum": '',
-        "channelId": '',
-        "serType": '',
-        "prvo": '',
-        "servalue": '',
-        "detailsUrl": '/survey/detailsofTomatoOrange.html?taskSheetId=',
-        'qnrId': args['questionnaireId'],
-        'h5PageUrlWeb': 'https://eva.customer.10086.cn/surv/QnrTomatoOrange.html?questionnaireId=%s' % args['questionnaireId'],
-        'token': args['token']
+class Poll():
+    proxies = {
+            # 'http': 'http://172.31.10.84:2345',
+            # 'https': 'http://172.31.10.84:2345'
     }
 
-    req_headers['Referer'] = jump302_url
-    s.headers.update(req_headers)
+    req_headers = {
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept': 'application/json, text/javascript, */*; q=0.01,',
+        'Connection': 'Keep-Alive',
+        'Host': 'eva.customer.10086.cn',
+        'Origin': 'https://eva.customer.10086.cn',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/79.0.3945.130 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    s = None
 
-    r = s.post(
-        'https://eva.customer.10086.cn/surveyH5Response/dynamicQuestion',
-        data=post_data,
-        verify=False
-    )
-    if r.status_code == 200:
-        del req_headers['Referer']
-        return 'msg:success, status_code:%s, url:%s, return_text:%s' % (r.status_code, url, r.text)
-        #return r.content
-    else:
-        return 'msg:failed, status_code:%s, url:%s, return_text:%s' % (r.status_code, url, r.text)
+    def __init__(self):
+        s = requests.session()
+        s.headers.update(self.req_headers)
+
+        if not len(self.proxies):
+            s.proxies = self.proxies
+        self.s = s
+
+    def submit_poll(self, url):
+        r = self.s.get(url, verify=False)
+        if '重复提交' in r.text:
+            return '%s 链接已提交,请勿重复提交' % url
+        if '请求超时' in r.text:
+            return '%s 链接已失效超时' % url
+
+        jump302_url = r.url
+        if 'token' in jump302_url:
+            args = self.get_url_args(jump302_url)
+        else:
+            return '%s 链接打开失败' % url
+        score_data = {
+            "qnrSuggestion": "",
+            "questionList": [
+                {
+                    "score": 10,
+                    "questionId": "2019112714221718701051001",
+                    "questionTitle": u"故障维修整体满意度",
+                    "answerOrder": 1,
+                    "surveyObjType": "0",
+                    "suggestion": "",
+                    "surveyLables": ""
+                },
+                {
+                    "score": 10,
+                    "questionId": "2019112714472810201052001",
+                    "questionTitle": u"上门及时性",
+                    "answerOrder": 2,
+                    "surveyObjType": "0",
+                    "suggestion": "",
+                    "surveyLables": u"上门快,上门准时,有提前预约时间"
+                },
+                {
+                    "score": 10,
+                    "questionId": "2019112714481805801051001",
+                    "questionTitle": u"故障解决专业性",
+                    "answerOrder": 3,
+                    "surveyObjType": "0",
+                    "suggestion": "",
+                    "surveyLables": u"故障解决快,现场干净整洁"
+                },
+                {
+                    "score": 10,
+                    "questionId": "2019112714504311401052001",
+                    "questionTitle": u"维修人员服务",
+                    "answerOrder": 4,
+                    "surveyObjType": "0",
+                    "suggestion": "",
+                    "surveyLables": u"服务态度好,答疑指导耐心,工装整洁大方"
+                },
+                {
+                    "score": "",
+                    "questionId": "2019112714514659501053001",
+                    "questionTitle": u"维修完成后是否测速",
+                    "suggestion": "",
+                    "surveyLables": u"是",
+                    "surveyObjType": "0",
+                    "answerOrder": 5
+                }
+            ]
+        }
+        post_data = {
+            "questionList": json.dumps(score_data, ensure_ascii=False),
+            "serNum": '',
+            "channelId": '',
+            "serType": '',
+            "prvo": '',
+            "servalue": '',
+            "detailsUrl": '/survey/detailsofTomatoOrange.html?taskSheetId=',
+            'qnrId': args['questionnaireId'],
+            'h5PageUrlWeb': 'https://eva.customer.10086.cn/surv/QnrTomatoOrange.html?questionnaireId=%s' % args[
+                'questionnaireId'],
+            'token': args['token']
+        }
+
+        self.req_headers['Referer'] = jump302_url
+        self.s.headers.update(self.req_headers)
+
+        r = self.s.post(
+            'https://eva.customer.10086.cn/surveyH5Response/dynamicQuestion',
+            data=post_data,
+            verify=False
+        )
+        if r.status_code == 200:
+            del self.req_headers['Referer']
+            return 'msg:success, status_code:%s, url:%s, return_text:%s' % (r.status_code, url, r.text)
+            # return r.content
+        else:
+            return 'msg:failed, status_code:%s, url:%s, return_text:%s' % (r.status_code, url, r.text)
+
+    def get_url_args(self, url):
+        args = url.split('?')[1].replace('&&', '&').split('&')
+        data = {}
+        for i in args:
+            x = i.split('=')
+            data[x[0]] = x[1]
+        return data
 
 
-def get_url_args(url):
-    args = url.split('?')[1].replace('&&', '&').split('&')
-    data = {}
-    for i in args:
-        x = i.split('=')
-        data[x[0]] = x[1]
-    return data
+class Communicate(QtCore.QObject):
+    signal = QtCore.pyqtSignal(str)
+
+class Ui_Form(object):
+    stopEvent = False
+    running = False
+
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(800, 600)
+        self.sig = Communicate()
+        self.sig.signal.connect(self.update_log)
+
+        self.form = Form
+        self.gridLayout = QtWidgets.QGridLayout(Form)
+        self.gridLayout.setObjectName("gridLayout")
+        self.frame = QtWidgets.QFrame(Form)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.frame.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.frame)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout_2.addItem(spacerItem, 2, 2, 1, 1)
+        self.btn_ok = QtWidgets.QPushButton(self.frame)
+        self.btn_ok.setObjectName("btn_ok")
+        self.gridLayout_2.addWidget(self.btn_ok, 2, 0, 1, 1)
+        self.groupBox_2 = QtWidgets.QGroupBox(self.frame)
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.gridLayout_4 = QtWidgets.QGridLayout(self.groupBox_2)
+        self.gridLayout_4.setObjectName("gridLayout_4")
+        self.txbLogs = QtWidgets.QPlainTextEdit(self.groupBox_2)
+        self.txbLogs.setObjectName("txbLogs")
+        self.txbLogs.setReadOnly(True)
+        self.gridLayout_4.addWidget(self.txbLogs, 0, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.groupBox_2, 1, 0, 1, 3)
+        self.groupBox = QtWidgets.QGroupBox(self.frame)
+        self.groupBox.setObjectName("groupBox")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.groupBox)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.txbList = QtWidgets.QPlainTextEdit(self.groupBox)
+        self.txbList.setObjectName("txbList")
+        self.gridLayout_3.addWidget(self.txbList, 0, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.groupBox, 0, 0, 1, 3)
+        self.btn_stop = QtWidgets.QPushButton(self.frame)
+        self.btn_stop.setObjectName("btn_stop")
+        self.gridLayout_2.addWidget(self.btn_stop, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.frame, 0, 0, 1, 1)
+
+        self.btn_ok.clicked.connect(self.btn_start_click_event)
+        self.btn_stop.clicked.connect(self.btn_stop_click_event)
+
+        self.msgBox = QMessageBox()
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def update_log(self, text):
+        self.txbLogs.appendPlainText(text)
 
 
-if __name__ == "__main__":
-    # 提前10分钟
-    pre_time = 10
-    # 文件名
-    file_name = 'data.txt'
-    # 日期时间格式
-    fmt = '%Y/%m/%d %H:%M'
+    def btn_stop_click_event(self):
+        self.stopEvent = True
+        self.running = False
 
-    with open(file_name, 'r', encoding='utf-8') as f:
-        text_data = f.read().replace('	', '|')
+    def btn_start_click_event(self):
+        # self.msgBox.about(self.form, 'test title', 'test content')
+        self.stopEvent = False
+        if self.running == True:
+            return
+            
+        text = self.txbList.toPlainText()
+        if not text.strip():
+            self.msgBox.about(self.form, u'通知', u'内容为空')
+            return
+        text = text.replace('	', '|')
+        text = re.sub(' +', ' ', text)
+        _thread.start_new_thread(self.poll_start, (text,))
 
-    text_data = re.sub(' +', ' ', text_data)
-    if not len(text_data):
-        exit('file is empty')
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "10086 poll"))
+        self.btn_ok.setText(_translate("Form", "Start"))
+        self.groupBox_2.setTitle(_translate("Form", "Logs"))
+        self.groupBox.setTitle(_translate("Form", "List"))
+        self.btn_stop.setText(_translate("Form", "Stop"))
 
-    lines = text_data.split('\n')
-    while True:
-        if not len(lines):
-            exit('done')
-        for line in lines:
-            if line.strip() == '':
-                lines.pop(0)
+    def poll_start(self, text):
+        # print(lines)
+        self.running = True
+        pre_time = 1
+        fmt = '%Y/%m/%d %H:%M'
+        poll = Poll()
+        success_list = []
+        data = []
+        for i in text.split('\n'):
+            if i[0:1] == '#' or i.strip() == '':
                 continue
+            else:
+                data.append(i)
 
-            x = line.split('|')
-            now = int(time.time())
-            end_date = time.mktime(time.strptime(x[0], fmt))
-            #print (end_date-now)
-            if end_date-now <= pre_time*60:
-                resp = submit_poll(x[1])
-                print(resp)
-                lines.pop(0)
+        while True:
+            if self.stopEvent:
+                break
+
+            #print(success_list)
+            if len(success_list) == len(data):
+                self.sig.signal.emit(u'执行结束')
+                self.stopEvent = True
+                self.running = False
+                
+            for line in data:
+                x = line.split('|')
+                now = int(time.time())
+                end_date = time.mktime(time.strptime(x[0], fmt))
+                is_start = end_date - now
+                print(is_start)
+                if is_start < pre_time*60 and is_start >0 and line not in success_list:
+                    resp_text = poll.submit_poll(x[1])
+                    #self.txbLogs.appendPlainText(resp_text)
+                    self.sig.signal.emit(resp_text)
+                    success_list.append(line)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    # init form
+    widget = QWidget()
+    # init ui_form
+    ui = Ui_Form()
+    ui.setupUi(widget)
+    # get window
+    qr = widget.frameGeometry()
+    # get screen center position
+    cp = QDesktopWidget().availableGeometry().center()
+    # move window to screen center
+    qr.moveCenter(cp)
+    widget.move(qr.topLeft())
+    # show window form
+    widget.show()
+
+    sys.exit(app.exec_())
