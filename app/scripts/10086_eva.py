1d0338068aacb5f1fe00aa3e249874efb036b8ee
dengguibao <tydgb@outlook.com>
2020-06-05 11:51:26 +0800

    update navigation ui

--- app/scripts/10086_eva.py
+++ app/scripts/10086_eva.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'main.ui'
+# Form implementation generated from reading ui file './main.ui'
 #
 # Created by: PyQt5 UI code generator 5.14.2
 #
 # WARNING! All changes made in this file will be lost!
 
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QMessageBox
 import sys
-
+from PyQt5 import QtCore, QtGui, QtWidgets
 import requests
 import re
 import time
 import json
 import urllib3
 import _thread
+
 urllib3.disable_warnings()
+
 
 class Poll():
     proxies = {
-            # 'http': 'http://172.31.10.84:2345',
-            # 'https': 'http://172.31.10.84:2345'
+        # 'http': 'http://172.31.10.84:2345',
+        # 'https': 'http://172.31.10.84:2345'
     }
 
     req_headers = {
@@ -149,7 +148,8 @@ class Poll():
 
 
 class Communicate(QtCore.QObject):
-    signal = QtCore.pyqtSignal(str)
+    signal = QtCore.pyqtSignal(list)
+
 
 class Ui_Form(object):
     stopEvent = False
@@ -157,59 +157,95 @@ class Ui_Form(object):
 
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(800, 600)
+        Form.resize(744, 545)
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.groupBox_2 = QtWidgets.QGroupBox(Form)
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.verticalLayout_7 = QtWidgets.QVBoxLayout(self.groupBox_2)
+        self.verticalLayout_7.setObjectName("verticalLayout_7")
+        self.txbList = QtWidgets.QPlainTextEdit(self.groupBox_2)
+        self.txbList.setObjectName("txbList")
+        self.verticalLayout_7.addWidget(self.txbList)
+        self.verticalLayout_4.addWidget(self.groupBox_2)
+        self.verticalLayout_2.addLayout(self.verticalLayout_4)
+        self.progressBar = QtWidgets.QProgressBar(Form)
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setObjectName("progressBar")
+        self.verticalLayout_2.addWidget(self.progressBar)
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.groupBox = QtWidgets.QGroupBox(Form)
+        self.groupBox.setObjectName("groupBox")
+        self.verticalLayout_6 = QtWidgets.QVBoxLayout(self.groupBox)
+        self.verticalLayout_6.setObjectName("verticalLayout_6")
+        self.txbLogs = QtWidgets.QPlainTextEdit(self.groupBox)
+        self.txbLogs.setBaseSize(QtCore.QSize(800, 600))
+        self.txbLogs.setReadOnly(True)
+        self.txbLogs.setObjectName("txbLogs")
+        self.verticalLayout_6.addWidget(self.txbLogs)
+        self.verticalLayout_5.addWidget(self.groupBox)
+        self.verticalLayout_2.addLayout(self.verticalLayout_5)
+        self.verticalLayout_3.addLayout(self.verticalLayout_2)
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.btn_ok = QtWidgets.QPushButton(Form)
+        self.btn_ok.setObjectName("btn_ok")
+        self.horizontalLayout_4.addWidget(self.btn_ok)
+        self.btn_stop = QtWidgets.QPushButton(Form)
+        self.btn_stop.setObjectName("btn_stop")
+        self.horizontalLayout_4.addWidget(self.btn_stop)
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setObjectName("label")
+        self.horizontalLayout_4.addWidget(self.label)
+        self.spinBox = QtWidgets.QSpinBox(Form)
+        self.spinBox.setProperty("value", 1)
+        self.spinBox.setObjectName("spinBox")
+        self.spinBox.setMaximum(60)
+        self.spinBox.setMinimum(1)
+        self.horizontalLayout_4.addWidget(self.spinBox)
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout_4.addWidget(self.label_2)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_4.addItem(spacerItem)
+        self.verticalLayout_3.addLayout(self.horizontalLayout_4)
+        self.verticalLayout_3.setStretch(0, 10)
+        self.verticalLayout_3.setStretch(1, 1)
+
         self.sig = Communicate()
         self.sig.signal.connect(self.update_log)
 
         self.form = Form
-        self.gridLayout = QtWidgets.QGridLayout(Form)
-        self.gridLayout.setObjectName("gridLayout")
-        self.frame = QtWidgets.QFrame(Form)
-        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame.setObjectName("frame")
-        self.frame.setFrameShape(QtWidgets.QFrame.NoFrame)
-        self.gridLayout_2 = QtWidgets.QGridLayout(self.frame)
-        self.gridLayout_2.setObjectName("gridLayout_2")
-        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.gridLayout_2.addItem(spacerItem, 2, 2, 1, 1)
-        self.btn_ok = QtWidgets.QPushButton(self.frame)
-        self.btn_ok.setObjectName("btn_ok")
-        self.gridLayout_2.addWidget(self.btn_ok, 2, 0, 1, 1)
-        self.groupBox_2 = QtWidgets.QGroupBox(self.frame)
-        self.groupBox_2.setObjectName("groupBox_2")
-        self.gridLayout_4 = QtWidgets.QGridLayout(self.groupBox_2)
-        self.gridLayout_4.setObjectName("gridLayout_4")
-        self.txbLogs = QtWidgets.QPlainTextEdit(self.groupBox_2)
-        self.txbLogs.setObjectName("txbLogs")
-        self.txbLogs.setReadOnly(True)
-        self.gridLayout_4.addWidget(self.txbLogs, 0, 0, 1, 1)
-        self.gridLayout_2.addWidget(self.groupBox_2, 1, 0, 1, 3)
-        self.groupBox = QtWidgets.QGroupBox(self.frame)
-        self.groupBox.setObjectName("groupBox")
-        self.gridLayout_3 = QtWidgets.QGridLayout(self.groupBox)
-        self.gridLayout_3.setObjectName("gridLayout_3")
-        self.txbList = QtWidgets.QPlainTextEdit(self.groupBox)
-        self.txbList.setObjectName("txbList")
-        self.gridLayout_3.addWidget(self.txbList, 0, 0, 1, 1)
-        self.gridLayout_2.addWidget(self.groupBox, 0, 0, 1, 3)
-        self.btn_stop = QtWidgets.QPushButton(self.frame)
-        self.btn_stop.setObjectName("btn_stop")
-        self.gridLayout_2.addWidget(self.btn_stop, 2, 1, 1, 1)
-        self.gridLayout.addWidget(self.frame, 0, 0, 1, 1)
-
+        self.msgBox = QtWidgets.QMessageBox()
         self.btn_ok.clicked.connect(self.btn_start_click_event)
         self.btn_stop.clicked.connect(self.btn_stop_click_event)
 
-        self.msgBox = QMessageBox()
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
-    def update_log(self, text):
-        self.txbLogs.appendPlainText(text)
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", u"10086问卷调查"))
+        self.groupBox_2.setTitle(_translate("Form", "列表"))
+        self.groupBox.setTitle(_translate("Form", "日志"))
+        self.btn_ok.setText(_translate("Form", "开始"))
+        self.btn_stop.setText(_translate("Form", "停止"))
+        self.label.setText(_translate("Form", u"       提前"))
+        self.label_2.setText(_translate("Form", "分钟"))
 
+    def update_log(self, val):
+        self.progressBar.setValue(val[0]*100)
+        self.txbLogs.appendPlainText(val[1])
 
     def btn_stop_click_event(self):
+        self.txbList.setReadOnly(False)
+        self.progressBar.setValue(0)
+        self.spinBox.setReadOnly(False)
         self.stopEvent = True
         self.running = False
 
@@ -218,7 +254,9 @@ class Ui_Form(object):
         self.stopEvent = False
         if self.running == True:
             return
-            
+        self.txbList.setReadOnly(True)
+        self.txbLogs.setPlainText('')
+        self.spinBox.setReadOnly(True)
         text = self.txbList.toPlainText()
         if not text.strip():
             self.msgBox.about(self.form, u'通知', u'内容为空')
@@ -227,18 +265,10 @@ class Ui_Form(object):
         text = re.sub(' +', ' ', text)
         _thread.start_new_thread(self.poll_start, (text,))
 
-    def retranslateUi(self, Form):
-        _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "10086 poll"))
-        self.btn_ok.setText(_translate("Form", "Start"))
-        self.groupBox_2.setTitle(_translate("Form", "Logs"))
-        self.groupBox.setTitle(_translate("Form", "List"))
-        self.btn_stop.setText(_translate("Form", "Stop"))
-
     def poll_start(self, text):
         # print(lines)
         self.running = True
-        pre_time = 1
+        pre_time = self.spinBox.value()
         fmt = '%Y/%m/%d %H:%M'
         poll = Poll()
         success_list = []
@@ -253,36 +283,38 @@ class Ui_Form(object):
             if self.stopEvent:
                 break
 
-            #print(success_list)
+            # print(success_list)
             if len(success_list) == len(data):
                 self.sig.signal.emit(u'执行结束')
                 self.stopEvent = True
                 self.running = False
-                
+
             for line in data:
                 x = line.split('|')
                 now = int(time.time())
                 end_date = time.mktime(time.strptime(x[0], fmt))
                 is_start = end_date - now
-                print(is_start)
-                if is_start < pre_time*60 and is_start >0 and line not in success_list:
+                # print(is_start)
+                if is_start < pre_time * 60 and is_start > 0 and line not in success_list:
                     resp_text = poll.submit_poll(x[1])
-                    #self.txbLogs.appendPlainText(resp_text)
-                    self.sig.signal.emit(resp_text)
+                    # self.txbLogs.appendPlainText(resp_text)
                     success_list.append(line)
+                    progress_percent = len(success_list)/len(data)
+                    self.sig.signal.emit([progress_percent, resp_text])
+
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     # init form
-    widget = QWidget()
+    widget = QtWidgets.QWidget()
     # init ui_form
     ui = Ui_Form()
     ui.setupUi(widget)
     # get window
     qr = widget.frameGeometry()
     # get screen center position
-    cp = QDesktopWidget().availableGeometry().center()
+    cp = QtWidgets.QDesktopWidget().availableGeometry().center()
     # move window to screen center
     qr.moveCenter(cp)
     widget.move(qr.topLeft())
